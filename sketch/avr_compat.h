64225c8943c98c1e4e6f87190f85b8b40436126e
unn4m3d <smelnikov871@gmail.com>
2019-02-05 16:33:45 +1000

    Совместимость с Arduino Uno

--- sketch/avr_compat.h
+++ sketch/avr_compat.h
@@ -1,12 +1,32 @@
 #pragma once
+/*
+Код для тестов на Arduino Uno
+*/
 
 #ifndef PA0
+  // Меняем пины PA0-PA3 на аппаратные ШИМ выходы Arduino
   #define PA0 3
   #define PA1 5
   #define PA2 6
   #define PA3 9
+
+  // Меняем пины на аналоговые
+  #define PA4 A0
+  #define PA5 A1
+  #define PB0 A2
+  #define PB1 A3
+
+  // Меняем пины на аппаратные ШИМ выходы
   #define PA6 10
   #define PA7 11
+
+  // Меняем пин на любой свободный цифровой (для переключателя)
+  #define PB10 13
+
+  // Меняем Serial1 на Serial
   #undef SERIAL
   #define SERIAL Serial
+
+  // Меняем режим для аналоговых пинов
+  #define INPUT_ANALOG INPUT
 #endif
