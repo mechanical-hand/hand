910304bdab71a796a21ce4b0ce7b61aa6058ee1e
unn4m3d <smelnikov871@gmail.com>
2019-02-05 15:15:01 +1000

    Вынесен код для тестов на не STM32 в отдельный файл

---
+++ sketch/avr_compat.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef PA0
+  #define PA0 3
+  #define PA1 5
+  #define PA2 6
+  #define PA3 9
+  #define PA6 10
+  #define PA7 11
+  #undef SERIAL
+  #define SERIAL Serial
+#endif
--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -8,16 +8,7 @@
 #define SERVO_EPSILON 3
 #define EXTEND_SPEED 50
 
-#ifndef PA0
-  #define PA0 3
-  #define PA1 5
-  #define PA2 6
-  #define PA3 9
-  #define PA6 10
-  #define PA7 11
-  #undef SERIAL
-  #define SERIAL Serial
-#endif
+#include "avr_compat.h"
 
 // Список машинок
 hand::servo servos[] = {
