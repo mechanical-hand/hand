f8fbcfbec69fdf9919aa3846c356eaef82322328
unn4m3d <smelnikov871@gmail.com>
2019-03-12 19:32:04 +1000

    Split

--- sketch/libraries/HandCore/servo.h
+++ sketch/libraries/HandCore/servo.h
@@ -1,5 +1,6 @@
 #pragma once
 #define NO_PIN 0
+#include <Servo.h>
 
 namespace hand
 {
@@ -9,105 +10,42 @@ namespace hand
     class servo
     {
     public:
-        servo(int pin, int initial, int angle) :
-            m_angle(angle),
-            m_pin(pin),
-            m_initial(initial),
-            m_min(-angle/2),
-            m_max(angle/2)
-        {
-            #ifndef INITIALIZE_SERVOS_IN_SETUP
-                init();
-            #endif
-        }
+        servo(int pin, int initial, int angle);
 
-        servo(int pin, int initial, int angle, int min, int max) :
-            m_angle(angle),
-            m_initial(initial),
-            m_pin(pin),
-            m_min(min),
-            m_max(max)
-        {
-            #ifndef INITIALIZE_SERVOS_IN_SETUP
-                init();
-            #endif
-        }
+        servo(int pin, int initial, int angle, int min, int max);
 
         /**
          * @brief Установка положения сервомашинки
          * @param value Желаемое значение
          */
-        void write(int value)
-        {
-            if(m_pin == NO_PIN) return;
-            m_servo.write(value);
-        }
+        void write(int value);
 
-        void init()
-        {
-            if(m_pin == NO_PIN) return;
-            m_servo.attach(m_pin);
-            writeDegrees(m_initial);
-        }
+        void init();
 
         /**
          * @brief Чтение текущего положения сервомашинки
          * @returns Положение сервомашинки
          */
-        int read()
-        {
-            if(m_pin == NO_PIN) return 0;
-            return m_servo.read();
-        }
+        int read();
 
-        bool writeDegrees(int degrees)
-        {
-            int half_angle = getHalfAngle();
-            int pwm = map(clamp(degrees), -half_angle, half_angle, 0, 180);
-            write(pwm);
-            return true;
-        }
+        bool writeDegrees(int degrees);
 
-        int readDegrees()
-        {
-            int half_angle = getHalfAngle();
-            int pwm = read();
-            return map(pwm, 0, 180, -half_angle, half_angle);
-        }
+        int readDegrees();
 
-        int getAngle()
-        {
-            return m_angle;
-        }
 
-        int getHalfAngle()
-        {
-            return m_angle/2;
-        }
 
-        int clamp(int value, int min, int max)
-        {
-            if(value > max) return max;
-            if(value < min) return min;
-            return value;
-        }
+        int clamp(int value, int min, int max);
 
-        int clamp(int value, bool use_restrictions)
-        {
-            int half = getHalfAngle();
-            int min = (use_restrictions ? -half : m_min);
-            int max = (use_restrictions ? half : m_max);
-            return clamp(value, min, max);
-        }
+        int clamp(int value, bool use_restrictions);
 
-        int clamp(int value)
-        {
-            return clamp(value, true);
-        }
+        int clamp(int value);
 
         int getMin(){ return m_min; }
         int getMax(){ return m_max; }
         int getPin(){ return m_pin; }
+        int getAngle(){ return m_angle; }
+
+        int getHalfAngle(){ return m_angle/2; }
     private:
         Servo m_servo;
         int m_angle, m_initial, m_pin, m_min, m_max;
